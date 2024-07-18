3309c7ee48c04ba68d8e5604e40263b6b7c36d36
retinotopic <retinotopic@proton.me>
2024-07-18 23:56:41 +0300

    ws connection managing

--- internal/hub/handlers.go
+++ internal/hub/handlers.go
@@ -3,6 +3,7 @@ package hub
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/fasthttp/websocket"
 	"github.com/retinotopic/go-bet/internal/auth"
@@ -20,6 +21,11 @@ func (h *hubPump) FindGame(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "error retrieving user", 500)
 	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	sub, err := prvdr.FetchUser(w, r)
 	if err != nil {
 		http.Error(w, "error retrieving user", 500)
@@ -29,16 +35,25 @@ func (h *hubPump) FindGame(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "error retrieving user", 500)
 		return
 	}
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	h.PlrMutex.RLock()
 	pl, ok := h.Players[user_id]
+	h.PlrMutex.RUnlock()
+
 	if !ok || ok && len(pl.URLlobby) == 0 {
 		pl = &lobby.PlayUnit{User_id: user_id, Name: name, Conn: conn}
 		h.ReqPlayers <- pl
-		// ping pong implementation here
+		h.keepAlive(conn, time.Second*15)
+
+		pl = &lobby.PlayUnit{}
+		err := conn.ReadJSON(pl)
+		if err != nil {
+			log.Println(err)
+		}
+		conn.Close()
+		if err != nil {
+			log.Println(err)
+		}
+
 	} else if ok && len(pl.URLlobby) != 0 {
 		pl.Conn.WriteJSON(pl)
 	}
@@ -62,13 +77,41 @@ func (h *hubPump) ConnectLobby(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
+	h.PlrMutex.RLock()
 	pl, ok := h.Players[user_id]
-	if !ok || ok && len(pl.URLlobby) == 0 {
-		pl = &lobby.PlayUnit{User_id: user_id, Name: name, Conn: conn}
-		h.ReqPlayers <- pl
-		// ping pong implementation here
-	} else if ok && len(pl.URLlobby) != 0 {
+	h.PlrMutex.RUnlock()
+
+	wsurl := r.URL.Path[7:]
+	if wsurl == pl.URLlobby && ok {
+		pl.Conn = conn
+		pl.Name = name
 		pl.Conn.WriteJSON(pl)
+		h.LMutex.RLock()
+		h.Lobby[pl.URLlobby].ConnHandle(pl)
+		h.LMutex.Unlock()
+	} else {
+		conn.Close()
 	}
 
+}
+func (h *hubPump) keepAlive(c *websocket.Conn, timeout time.Duration) {
+	lastResponse := time.Now()
+	c.SetPongHandler(func(msg string) error {
+		lastResponse = time.Now()
+		return nil
+	})
+
+	go func() {
+		for {
+			err := c.WriteMessage(websocket.PingMessage, []byte("keepalive"))
+			if err != nil {
+				return
+			}
+			time.Sleep(timeout / 2)
+			if time.Since(lastResponse) > timeout {
+				c.Close()
+				return
+			}
+		}
+	}()
 }
