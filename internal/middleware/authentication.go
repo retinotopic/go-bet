6f48ddf5cc43aa5f65fcbcdb7f907e40b9864e69
retinotopic <retinotopic@proton.me>
2024-07-31 22:57:22 +0300

    middleware no longer depends on global variable

--- internal/middleware/authentication.go
+++ internal/middleware/authentication.go
@@ -5,29 +5,35 @@ import (
 	"strconv"
 
 	"github.com/google/uuid"
-	"github.com/retinotopic/go-bet/internal/auth"
-	"github.com/retinotopic/go-bet/internal/db"
+	"github.com/retinotopic/tinyauth/provider"
 )
 
-func FetchUser(next http.Handler) http.Handler {
+type UserMiddleware struct {
+	GetUser     func(string) (int, string, error)
+	GetProvider func(http.ResponseWriter, *http.Request) (provider.Provider, error)
+	WriteCookie func(http.ResponseWriter, string) *http.Cookie
+	ReadCookie  func(*http.Request) (string, error)
+}
+
+func (um *UserMiddleware) FetchUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var user_id int
 		var name, ident string
-		prvdr, err := auth.Mproviders.GetProvider(w, r)
+		prvdr, err := um.GetProvider(w, r)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
 		sub, err := prvdr.FetchUser(w, r)
 		if err != nil {
-			name, err = auth.ReadCookie(r)
+			name, err = um.ReadCookie(r)
 			if err != nil {
 				name = uuid.New().String()
-				auth.WriteCookie(w, name)
+				um.WriteCookie(w, name)
 			}
 			ident = name
 
 		} else {
-			user_id, name, err = db.GetUser(sub)
+			user_id, name, err = um.GetUser(sub)
 			ident = strconv.Itoa(user_id)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
