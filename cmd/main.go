fefa97b8f18c0aa27a80d654d16377a0c70bd1d4
retinotopic <retinotopic@proton.me>
2024-08-01 23:57:09 +0300

    configuration of authentication providers is created via cmd.go

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,9 @@ import (
 
 	"github.com/retinotopic/go-bet/internal/queue"
 	"github.com/retinotopic/go-bet/internal/router"
+	"github.com/retinotopic/tinyauth/provider"
+	"github.com/retinotopic/tinyauth/providers/firebase"
+	"github.com/retinotopic/tinyauth/providers/google"
 )
 
 func main() {
@@ -26,9 +29,24 @@ func main() {
 		log.Fatalf("Error unmarshaling JSON: %v", err)
 	}
 
+	mp := make(map[string]provider.Provider)
+	google, err := google.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET"), os.Getenv("REDIRECT"), "/refresh")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	firebase, err := firebase.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET"), os.Getenv("REDIRECT"), "refresh")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// with the issuer claim, we know which provider issued token.
+	mp[os.Getenv("ISSUER_FIREBASE")] = firebase
+	mp[os.Getenv("ISSUER_GOOGLE")] = google
+	mp[os.Getenv("ISSUER2_GOOGLE")] = google
+
 	flag.Parse()
 	fmt.Println(*addr)
-	srv := router.NewRouter(*addr, *addrQueue, config)
+	srv := router.NewRouter(*addr, *addrQueue, config, mp)
 	err = srv.Run()
 	if err != nil {
 		log.Println(err)
--- internal/router/router.go
+++ internal/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"net/http"
 
+	"github.com/retinotopic/go-bet/internal/auth"
 	"github.com/retinotopic/go-bet/internal/hub"
 	"github.com/retinotopic/go-bet/internal/queue"
 )
@@ -12,10 +13,11 @@ type Router struct {
 	AddrQueue   string
 	ConfigQueue queue.Config
 	Queue       *queue.TaskQueue
+	Auth        auth.ProviderMap
 }
 
-func NewRouter(addr string, addrQueue string, config queue.Config) *Router {
-	return &Router{Addr: addr, AddrQueue: addrQueue, ConfigQueue: config}
+func NewRouter(addr string, addrQueue string, config queue.Config, mp auth.ProviderMap) *Router {
+	return &Router{Addr: addr, AddrQueue: addrQueue, ConfigQueue: config, Auth: mp}
 }
 func (r *Router) Run() error {
 	var err error
@@ -28,8 +30,8 @@ func (r *Router) Run() error {
 
 	mux.HandleFunc("/lobby", hub.ConnectLobby)
 	mux.HandleFunc("/findgame", hub.FindGame)
-	//mux.HandleFunc("/beginauth", )
-	//mux.HandleFunc("/completeauth", )
+	mux.HandleFunc("/beginauth", r.Auth.BeginAuth)
+	mux.HandleFunc("/completeauth", r.Auth.CompleteAuth)
 	err = http.ListenAndServe(r.Addr, mux)
 	if err != nil {
 		return err
