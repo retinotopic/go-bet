f457b1872ea6a1714fa07800688d4cb05cf62fac
retinotopic <retinotopic@proton.me>
2024-09-18 22:49:08 +0300

    fewer function calls when starting a game

--- internal/hub/lobby/game.go
+++ internal/hub/lobby/game.go
@@ -43,83 +43,78 @@ func (g *Game) tillTurnOrBlind() {
 		}
 	}
 }
+
 func (g *Game) Game() {
 	for {
 		select {
 		case <-g.StartGameCh:
-			g.StartGame()
+			var stages int
+			g.stop = make(chan bool, 1)
+			randsrc := rand.NewSource(int64(new(maphash.Hash).Sum64()))
+			rand.New(randsrc).Shuffle(len(g.PlayersRing.Players), func(i, j int) {
+				g.Players[i], g.Players[j] = g.Players[j], g.Players[i]
+			})
+			g.DealNewHand()
+			g.TurnTicker = time.NewTicker(time.Second * 1)
+			g.BlindTimer = time.NewTimer(time.Minute * 5)
+			go g.tillTurnOrBlind()
+			defer func() { // prevent goroutine leakage
+				g.stop <- true
+			}()
+			for GAME_LOOP := true; GAME_LOOP; {
+				select {
+				case pl, ok := <-g.PlayerCh:
+					if pl.Place == g.PlayersRing.Idx && ok {
+						g.TurnTicker.Stop()
+
+						pl.Bet = pl.Bet + pl.CtrlBet
+						pl.Bankroll -= pl.CtrlBet
+
+						if pl.Bet >= g.MaxBet {
+							g.MaxBet = pl.Bet
+						} else {
+							pl.IsFold = true
+						}
+
+						g.BroadcastCh <- pl
+
+						pl.HasActed = true
+						g.PlayersRing.Next(1)
+						for pl.IsFold {
+							pl = g.Players[g.PlayersRing.Next(1)]
+						}
+						if pl.Bet == g.MaxBet && pl.HasActed {
+							switch stages {
+							case flop: // preflop to flop
+								flopcard := g.Deck.Draw(3)
+								g.Board.Cards = append(g.Board.Cards, flopcard...)
+								g.BroadcastCh <- g.Board
+							case turn, river: // flop to turn, turn to river
+								flopcard := g.Deck.Draw(1)
+								g.Board.Cards = append(g.Board.Cards, flopcard[0])
+								g.BroadcastCh <- g.Board
+							case postriver:
+								if ok := g.PostRiver(); ok {
+									GAME_LOOP = false
+									break
+								}
+								stages = -1
+							}
+							for _, v := range g.Players {
+								v.HasActed, v.IsFold = false, false
+							}
+							stages++
+						}
+
+						g.TurnTicker.Reset(time.Second * 30)
+					}
+				case <-g.Shutdown:
+					GAME_LOOP = false
+					break
+				}
+			}
 			g.ValidateCh <- Ctrl{plr: g.Board}
 		case <-g.PlayerCh:
-		case <-g.Shutdown:
-			return
-		}
-	}
-
-}
-func (g *Game) StartGame() {
-	for len(g.PlayerCh) > 0 {
-		<-g.PlayerCh
-	}
-	var stages int
-	g.stop = make(chan bool, 1)
-	randsrc := rand.NewSource(int64(new(maphash.Hash).Sum64()))
-	rand.New(randsrc).Shuffle(len(g.PlayersRing.Players), func(i, j int) {
-		g.Players[i], g.Players[j] = g.Players[j], g.Players[i]
-	})
-	g.DealNewHand()
-
-	g.TurnTicker = time.NewTicker(time.Second * 1)
-	g.BlindTimer = time.NewTimer(time.Minute * 5)
-	go g.tillTurnOrBlind()
-	defer func() { // prevent goroutine leakage
-		g.stop <- true
-	}()
-	for {
-		select {
-		case pl, ok := <-g.PlayerCh:
-			if pl.Place == g.PlayersRing.Idx && ok {
-				g.TurnTicker.Stop()
-
-				pl.Bet = pl.Bet + pl.CtrlBet
-				pl.Bankroll -= pl.CtrlBet
-
-				if pl.Bet >= g.MaxBet {
-					g.MaxBet = pl.Bet
-				} else {
-					pl.IsFold = true
-				}
-
-				g.BroadcastCh <- pl
-
-				pl.HasActed = true
-				g.PlayersRing.Next(1)
-				for pl.IsFold {
-					pl = g.Players[g.PlayersRing.Next(1)]
-				}
-				if pl.Bet == g.MaxBet && pl.HasActed {
-					switch stages {
-					case flop: // preflop to flop
-						flopcard := g.Deck.Draw(3)
-						g.Board.Cards = append(g.Board.Cards, flopcard...)
-						g.BroadcastCh <- g.Board
-					case turn, river: // flop to turn, turn to river
-						flopcard := g.Deck.Draw(1)
-						g.Board.Cards = append(g.Board.Cards, flopcard[0])
-						g.BroadcastCh <- g.Board
-					case postriver:
-						if ok := g.PostRiver(); ok {
-							return
-						}
-						stages = -1
-					}
-					for _, v := range g.Players {
-						v.HasActed, v.IsFold = false, false
-					}
-					stages++
-				}
-
-				g.TurnTicker.Reset(time.Second * 30)
-			}
 		case <-g.Shutdown:
 			return
 		}
